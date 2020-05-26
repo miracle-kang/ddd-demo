173ab3c8be61853d8b5411a789ed8e79788d1b70
Miracle <18365050+miracle-kang@users.noreply.github.com>
2020-05-26 22:43:48 +0800

    Swagger-UI path matcher pattern

---
+++ swagger-ui/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+java -Duser.timezone=$TZ $JAVA_OPTS -jar "$1" \
+     --spring.cloud.config.uri="$CONFIG_URI" \
+     --eureka.client.serviceUrl.defaultZone="$EUREKA_SERVICE_URL" \
+     --spring.zipkin.base-url="$ZIPKIN_BASE_URL"
